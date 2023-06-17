45026da90078ee9dae8c09c3763c1c66ee11348a
MatosOliveira <matosalem.oliveira@gmail.com>
2023-06-17 20:17:42 -0300

    Inclusão da interface e implementação nas classes existentes.

---
+++ src/model/services/TaxService.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package model.services;
+
+/**
+ * Interface - Impostos
+ * 
+ * @author Matos - 17.06.2023
+ *
+ */
+public interface TaxService {
+	
+	Double taxService(Double amount);
+}
